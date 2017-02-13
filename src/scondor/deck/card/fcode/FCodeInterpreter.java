16927e2a38acd43a54fca79d068a245a8db28de3
Thomas Langs <Thomas Langs@DESKTOP-JPM23JL>
2017-02-13 20:23:18 +0100

    ^test

--- src/scondor/deck/card/fcode/FCodeInterpreter.java
+++ src/scondor/deck/card/fcode/FCodeInterpreter.java
@@ -28,41 +28,42 @@ public class FCodeInterpreter {
 	}
 	
 	public void triger(){
-		for(counter=0;!(fcodeline[counter].isEmpty());counter++)
-			//Label
-			if(fcodeline[counter].startsWith("- ")){
-				labels.add(new Label(fcodeline[counter].substring(2),counter));
-			// Variable declaration
-			}else if(fcodeline[counter].startsWith("var")){
-				bufferarray=fcodeline[counter].split(" ");
-				vars.add(new Variable(bufferarray[1],Integer.parseInt(bufferarray[2])));
-			// Variable edit using offset value
-			}else if(fcodeline[counter].startsWith("editvar")){
-				bufferarray=fcodeline[counter].split("(")[1].split(")")[0].split(",");
-				getVar(bufferarray[0]).editValue(Integer.parseInt(bufferarray[1]));
-			//Variable edit using absolute value
-			}else if(fcodeline[counter].startsWith("setvar")){
-				bufferarray=fcodeline[counter].split("(")[1].split(")")[0].split(",");
-				getVar(bufferarray[0]).setValue(Integer.parseInt(bufferarray[1]));
-			//Jump command
-			}else if(fcodeline[counter].startsWith("jump")){
-				counter=getLabel(fcodeline[counter].split(" ")[1]).getValue();
-			// get Attack of Enemy
-			}else if(fcodeline[counter].startsWith("getEnemyAttack")){
-				getVar((fcodeline[counter].split("(")[1].split(")")[0])).setValue(((TroopCardData)t.getCard()).getAttack());
-			// set Attack of Enemy
-			}else if(fcodeline[counter].startsWith("setEnemyAttack")){
-				((TroopCardData) t.getCard()).setAttack(getVar(fcodeline[counter].split("(")[1].split(")")[0]).getValue());
-			// set Attack of Enemy using offset value
-			}else if(fcodeline[counter].startsWith("editEnemyAttack")){
-				((TroopCardData)t.getCard()).setAttack(((TroopCardData)t.getCard()).getAttack()+(getVar(fcodeline[counter].split("(")[1].split(")")[0]).getValue()));
-			//if query
-			}else if(fcodeline[counter].startsWith("if")){
-				bufferarray=fcodeline[counter].split("(")[1].split(")")[0].split(",")[0].split("==");
-				if(getVar(bufferarray[0]).getValue()==Integer.parseInt(bufferarray[1])){
-					counter=Integer.parseInt(fcodeline[counter].split("(")[1].split(")")[0].split(",")[1]);
+		for(counter=0;!(fcodeline[counter].isEmpty());counter++){
+				//Label
+				if(fcodeline[counter].startsWith("- ")){
+					labels.add(new Label(fcodeline[counter].substring(2),counter));
+				// Variable declaration
+				}else if(fcodeline[counter].startsWith("var")){
+					bufferarray=fcodeline[counter].split(" ");
+					vars.add(new Variable(bufferarray[1],Integer.parseInt(bufferarray[2])));
+				// Variable edit using offset value
+				}else if(fcodeline[counter].startsWith("editvar")){
+					bufferarray=fcodeline[counter].split("(")[1].split(")")[0].split(",");
+					getVar(bufferarray[0]).editValue(Integer.parseInt(bufferarray[1]));
+				//Variable edit using absolute value
+				}else if(fcodeline[counter].startsWith("setvar")){
+					bufferarray=fcodeline[counter].split("(")[1].split(")")[0].split(",");
+					getVar(bufferarray[0]).setValue(Integer.parseInt(bufferarray[1]));
+				//Jump command
+				}else if(fcodeline[counter].startsWith("jump")){
+					counter=getLabel(fcodeline[counter].split(" ")[1]).getValue();
+				// get Attack of Enemy
+				}else if(fcodeline[counter].startsWith("getEnemyAttack")){
+					getVar((fcodeline[counter].split("(")[1].split(")")[0])).setValue(((TroopCardData)t.getCard()).getAttack());
+				// set Attack of Enemy
+				}else if(fcodeline[counter].startsWith("setEnemyAttack")){
+					((TroopCardData) t.getCard()).setAttack(getVar(fcodeline[counter].split("(")[1].split(")")[0]).getValue());
+				// set Attack of Enemy using offset value
+				}else if(fcodeline[counter].startsWith("editEnemyAttack")){
+					((TroopCardData)t.getCard()).setAttack(((TroopCardData)t.getCard()).getAttack()+(getVar(fcodeline[counter].split("(")[1].split(")")[0]).getValue()));
+				//if query
+				}else if(fcodeline[counter].startsWith("if")){
+					bufferarray=fcodeline[counter].split("(")[1].split(")")[0].split(",")[0].split("==");
+					if(getVar(bufferarray[0]).getValue()==Integer.parseInt(bufferarray[1])){
+						counter=Integer.parseInt(fcodeline[counter].split("(")[1].split(")")[0].split(",")[1]);
+					}
 				}
-			}
+		}
 	}
 	
 	public Variable getVar(String name) {
